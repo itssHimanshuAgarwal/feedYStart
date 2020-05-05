f7a325ac1c35389dcf2a5219509bb6f0dfe068d5
HIMANSHU RAMESH AGARWAL <49142525+himanshuagarwal12@users.noreply.github.com>
2020-05-06 04:10:45 +0530

    Add files via upload

---
+++ client/src/components/App.js
@@ -0,0 +1,33 @@
+import "materialize-css/dist/css/materialize.min.css";
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+import Header from "./Header";
+import Landing from "./Landing";
+const DashBoard = () => <h2>DashBoard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+//collection of the routes
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={DashBoard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
---
+++ client/src/components/Payments.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class Payments extends Component {
+  render() {
+    debugger;
+    //actual javascript code appearance
+    return (
+      <StripeCheckout
+        name="feedYstart"
+        description="$5 for 5 credits"
+        amount={500}
+        token={(token) => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default connect(null, actions)(Payments);
