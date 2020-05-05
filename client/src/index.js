1d26b82776d6448918b25a13c8f90cb6d8377697
HIMANSHU RAMESH AGARWAL <49142525+himanshuagarwal12@users.noreply.github.com>
2020-05-06 04:05:02 +0530

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import "materialize-css/dist/css/materialize.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+
+import App from "./components/App";
+import reducers from "./reducers";
+
+import axios from "axios";
+window.axios = axios;
+
+//development only axios helpers!!
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
